b5295c6e7b01607d4328baaeac79752836355387
LeoProk <just_beta@hotmail.com>
2020-04-18 23:20:58 +0300

    init

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-
-import { HomePage } from './home.page';
+import { RouterModule } from '@angular/router'; 
+import { HomePage } from './home.page'; 
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    HttpClientModule,
     RouterModule.forChild([
       {
         path: '',
